537a935560726cfa06aca520e8c9a4320e13224d
Wojack <Szwujj@gmail.com>
2026-02-10 17:18:09 -0500

    Fix tests to call main() function directly

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,13 +5,13 @@ from main import main as app_main
 def test_main_default(capsys, monkeypatch):
     # Run with no args -> should greet World
     monkeypatch.setattr(sys, 'argv', ['main.py'])
-    app_main.main()
+    app_main()
     captured = capsys.readouterr()
     assert 'Hello, World! funStaff is ready.' in captured.out
 
 
 def test_main_name(capsys, monkeypatch):
     monkeypatch.setattr(sys, 'argv', ['main.py', '--name', 'Jacob'])
-    app_main.main()
+    app_main()
     captured = capsys.readouterr()
     assert 'Hello, Jacob! funStaff is ready.' in captured.out
